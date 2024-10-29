5f7598aada3b67709c92dee01680a8f6cfee40b6
Gael <gaelfranciscoM.47@gmail.com>
2024-10-29 17:27:32 -0600

    Cambio en el pin

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, JavaBank ATM version control with Git");
+        ///
+        String accountNumber = "123456789";
+        double balance = 1500.75;
+        int pin = 1234;
+
+        // Array de montos de transacciones
+        int[] transactionAmounts = {200, -100, 50};
+
+        // Operaciones con variables
+        balance += transactionAmounts[0]; // Depósito
+        if (balance > 0 && pin == 1234) {
+            System.out.println("Acceso Concedido");
+        }
+
+        // Uso de operadores
+        balance++; // Incremento
+        String status = (balance < 0) ? "Deuda" : "Crédito";
+        System.out.println("Estado de cuenta: " + status);
     }
 }
