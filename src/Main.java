b9a8631b7390cd853edf436564165e85caed6baf
Gael <gaelfranciscoM.47@gmail.com>
2024-11-05 08:56:45 -0600

    Ejemplo de como funcionaria un banco

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,79 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, JavaBank ATM version control with Git");
-        ///
-        String accountNumber = "123456789";
-        double balance = 1500.75;
-        int pin = 12345;
+        System.out.println("Opciones del ATM:");
+        System.out.println("1. Ingresar");
+        System.out.println("2. Salir");
+        System.out.println("Selecciona una opción:");
 
-        // Array de montos de transacciones
-        int[] transactionAmounts = {200, -100, 50};
+        Scanner sc = new Scanner(System.in);
+        int option = sc.nextInt();
 
-        // Operaciones con variables
-        balance += transactionAmounts[0]; // Depósito
-        if (balance > 0 && pin == 12345) {
-            System.out.println("Acceso Concedido");
+        switch (option) {
+            case 1:
+                checkLogin();
+                break;
+            case 2:
+                getOut();
+                break;
+            default:
+                System.out.println("Opción no válida.");
         }
+    }
 
-        // Uso de operadores
-        balance++; // Incremento
-        String status = (balance < 0) ? "Deuda" : "Crédito";
-        System.out.println("Estado de cuenta: " + status);
+    private static void checkLogin() {
+        Scanner cl = new Scanner(System.in);
+        int attemps = 0;
+        int pin = 1234;
+        System.out.println("Intoduzca su pin: ");
+        int userpin = cl.nextInt();
+        while (attemps < 3) {
+            if (pin == userpin) {
+                System.out.println("\nBienvenido :)\n");
+                Scanner as = new Scanner(System.in);
+                System.out.println("Selecciona una opcion: ");
+                System.out.println("1. Consultar cuenta");
+                System.out.println("2. Depositar efectivo");
+                System.out.println("3. Retirar efectivo");
+                int secondList = as.nextInt();
+
+                switch (secondList) {
+                    case 1:
+                        checkBalance();
+                        break;
+                    case 2:
+                        depositMoney();
+                        break;
+                    case 3:
+                        withdrawMoney();
+                        break;
+                    default:
+                        System.out.println("Opcion no valida");
+                }
+            } else {
+                System.out.println("Error intoduzca su pin nuevamente: ");
+                cl.nextInt();
+            }
+            attemps++;
+        }
+        System.out.println("Ha exedido el limite de intentos");
+    }
+
+    private static void getOut() {
+        System.out.println("Adios");
+    }
+
+    private static void checkBalance() {
+        System.out.println("Estamos trabajando en el proceso");
+
+    }
+
+    private static void depositMoney() {
+        System.out.println("Estas trabajando en el proceso");
+    }
+
+    private static void withdrawMoney() {
+        System.out.println("Solo billetes de 100");
     }
 }
